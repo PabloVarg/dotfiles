b528829fe9f367287330c904f4f53f24d1ed29b7
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-10-27 16:39:24 -0600

    feat(nvim-vimspector): Configure mappings

--- .config/nvim/after/plugin/vimspector.lua
+++ .config/nvim/after/plugin/vimspector.lua
@@ -1,19 +1,28 @@
-vim.keymap.set("n", "<leader>d;", "<Plug>VimspectorContinue")
-vim.keymap.set("n", "<leader>d,", "<Plug>VimspectorStop")
-vim.keymap.set("n", "<leader>d.", "<Plug>VimspectorRestart")
-vim.keymap.set("n", "<leader>dw", "<Plug>VimspectorPause")
+vim.keymap.set("n", "<leader>db", vim.fn["vimspector#ListBreakpoints"])
+vim.keymap.set("n", "<leader>dc", function()
+	vim.call("vimspector#ClearBreakpoints")
+end)
 
-vim.keymap.set("n", "<leader>dx", vim.cmd.VimspectorReset)
+vim.keymap.set("n", "<leader>dl", vim.fn["vimspector#GoToCurrentLine"])
+vim.keymap.set("n", "<leader>dd", vim.fn["vimspector#JumpToProgramCounter"])
+vim.keymap.set("n", "<leader>dtc", vim.fn["vimspector#RunToCursor"])
+vim.keymap.set("n", "<leader>de", vim.fn["vimspector#StepOver"])
+vim.keymap.set("n", "<leader>di", vim.fn["vimspector#StepInto"])
+vim.keymap.set("n", "<leader>do", vim.fn["vimspector#StepOut"])
 
-vim.keymap.set("n", "<leader>tb", "<Plug>VimspectorToggleBreakpoint")
+-- vim.keymap.set("n", "<leader>dx", vim.fn["vimspector#Launch"])
+vim.keymap.set("n", "<leader>dx", vim.fn["vimspector#Continue"])
+vim.keymap.set("n", "<leader>dq", vim.cmd.VimspectorReset)
+vim.keymap.set("n", "<leader>dr", vim.fn["vimspector#Restart"])
+
+vim.keymap.set("n", "<leader>tb", vim.fn["vimspector#ToggleBreakpoint"])
 vim.keymap.set("n", "<leader>tc", "<Plug>VimspectorToggleConditionalBreakpoint")
 vim.keymap.set("n", "<leader>dpf", "<Plug>VimspectorAddFunctionBreakpoint")
 
-vim.keymap.set("n", "<leader>dru", "<Plug>VimspectorRunToCursor")
-vim.keymap.set("n", "<leader>dre", "<Plug>VimspectorStepOver")
-vim.keymap.set("n", "<leader>dri", "<Plug>VimspectorStepInto")
-vim.keymap.set("n", "<leader>dro", "<Plug>VimspectorStepOut")
-
 vim.keymap.set("n", "<leader>dk", "<Plug>VimspectorUpFrame")
 vim.keymap.set("n", "<leader>dj", "<Plug>VimspectorDownFrame")
+
 vim.keymap.set({ "x", "n" }, "<leader>di", "<Plug>VimspectorBalloonEval")
+
+vim.keymap.set("n", "[b", vim.fn["vimspector#JumpToPreviousBreakpoint"])
+vim.keymap.set("n", "[b", vim.fn["vimspector#JumpToNextBreakpoint"])
