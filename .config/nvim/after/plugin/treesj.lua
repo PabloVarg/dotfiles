bcd2ca8850116862211025c4215ac5f47953509f
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-11-28 20:44:13 -0600

    feat(nvim-treesj): Increment max_join_length

--- .config/nvim/after/plugin/treesj.lua
+++ .config/nvim/after/plugin/treesj.lua
@@ -2,6 +2,7 @@ local treesj = require("treesj")
 
 treesj.setup({
 	use_default_keymaps = false,
+	max_join_length = 10000,
 })
 
-vim.keymap.set({ "n", "x" }, "gS", treesj.toggle)
+vim.keymap.set("n", "gS", treesj.toggle)
