f238d8addd7f50a8a60dd4525412d2efa1896b2c
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-12-09 23:36:00 -0600

    refactor(nvim-commands): Extract autocmd's to separate file

--- .config/nvim/lua/me/init.lua
+++ .config/nvim/lua/me/init.lua
@@ -1,34 +1,4 @@
+require("me.commads")
 require("me.remaps")
 require("me.sets")
 require("me.lazy")
-
-local augroup = vim.api.nvim_create_augroup("base", {})
-
-vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
-	group = augroup,
-	pattern = "*",
-	command = [[:silent! !]],
-})
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-	group = augroup,
-	pattern = "*",
-	command = [[setlocal formatoptions -=ro]],
-})
-
-vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-	group = augroup,
-	pattern = "*",
-	callback = function()
-		vim.highlight.on_yank({
-			timeout = 50,
-		})
-	end,
-})
-
-vim.api.nvim_create_autocmd({ "VimResized" }, {
-	group = augroup,
-	callback = function()
-		vim.cmd("tabdo wincmd =")
-	end,
-})
