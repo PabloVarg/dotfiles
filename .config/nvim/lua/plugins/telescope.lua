d4e582ce31633687253013031b17d1fc3ad474df
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-12-10 22:17:05 -0600

    feat(nvim-telescope): Live grep in hidden files as well

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -36,6 +36,17 @@ return {
 					find_files = {
 						no_ignore = true,
 					},
+					live_grep = {
+						additional_args = {
+							"--hidden",
+						},
+					},
+					grep_string = {
+						additional_args = {
+							"--hidden",
+						},
+					},
+				},
 				extensions = {
 					fzf = {},
 				},
