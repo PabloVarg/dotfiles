9e23eba414a3c2426060f3f483d1e5950af3542c
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-12-18 23:14:36 -0600

    feat(nvim-repeat): Remove vim-repeat

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,7 +3,6 @@ return {
 		{ "stevearc/dressing.nvim", event = "VeryLazy" },
 		"christoomey/vim-tmux-navigator",
 		"tpope/vim-commentary",
-		"tpope/vim-repeat",
 		"theprimeagen/git-worktree.nvim",
 		"mattn/emmet-vim",
 	},
