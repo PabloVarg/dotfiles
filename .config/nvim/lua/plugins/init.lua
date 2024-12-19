f1a6f4c3c9734f81e1caf4ff9c5a3e990a4b2f08
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-12-18 23:19:48 -0600

    refactor(nvim-plugins): Reorder simple plugins

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,9 +1,9 @@
 return {
 	{
-		{ "stevearc/dressing.nvim", event = "VeryLazy" },
 		"christoomey/vim-tmux-navigator",
 		"tpope/vim-commentary",
 		"theprimeagen/git-worktree.nvim",
+		{ "stevearc/dressing.nvim", event = "VeryLazy" },
 		{ "mattn/emmet-vim", ft = "html,css" },
 	},
 }
