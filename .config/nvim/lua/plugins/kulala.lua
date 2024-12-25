26e3ccd28f5e66a91ae6a6fc2b9ce278e686a9ce
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-12-24 20:14:33 -0600

    fix(nvim-http): Apply keymaps only on current buffer

--- .config/nvim/lua/plugins/kulala.lua
+++ .config/nvim/lua/plugins/kulala.lua
@@ -1,21 +1,27 @@
 return {
 	{
 		"mistweaverco/kulala.nvim",
-		enabled = false,
+		enabled = true,
 		ft = "http",
 		config = function()
 			require("kulala").setup({
 				default_view = "headers_body",
 			})
 
-			vim.keymap.set("n", "<leader>ex", require("kulala").run)
-			vim.keymap.set("n", "<leader>eu", require("kulala").run_all)
-			vim.keymap.set("n", "<leader>et", require("kulala").search)
-			vim.keymap.set("n", "<leader>ei", require("kulala").inspect)
-			vim.keymap.set("n", "<leader>es", require("kulala").show_stats)
-			vim.keymap.set("n", "<leader>ey", require("kulala").copy)
-			vim.keymap.set("n", "<leader>ec", require("kulala").from_curl)
-			vim.keymap.set("n", "<leader>ev", require("kulala").set_selected_env)
+			vim.api.nvim_create_autocmd({ "FileType" }, {
+				group = vim.api.nvim_create_augroup("kulala", { clear = true }),
+				pattern = "http",
+				callback = function(ev)
+					vim.keymap.set("n", "<leader>ex", require("kulala").run, { buffer = ev.buf })
+					vim.keymap.set("n", "<leader>eu", require("kulala").run_all, { buffer = ev.buf })
+					vim.keymap.set("n", "<leader>et", require("kulala").search, { buffer = ev.buf })
+					vim.keymap.set("n", "<leader>ei", require("kulala").inspect, { buffer = ev.buf })
+					vim.keymap.set("n", "<leader>es", require("kulala").show_stats, { buffer = ev.buf })
+					vim.keymap.set("n", "<leader>ey", require("kulala").copy, { buffer = ev.buf })
+					vim.keymap.set("n", "<leader>ec", require("kulala").from_curl, { buffer = ev.buf })
+					vim.keymap.set("n", "<leader>ev", require("kulala").set_selected_env, { buffer = ev.buf })
+				end,
+			})
 		end,
 	},
 }
