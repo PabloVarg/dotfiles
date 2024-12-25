755399c27ec862d0a08b2b3ac2038eeb1738414c
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-12-24 20:03:28 -0600

    feat(nvim-lua): Apply keymap to current buffer

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -1,5 +1,5 @@
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
-vim.keymap.set("n", "<leader>ex", ":.lua<CR>")
-vim.keymap.set("x", "<leader>ex", ":lua<CR>")
-vim.keymap.set("n", "<leader>eu", ":source %<CR>")
+vim.keymap.set("n", "<leader>ex", ":.lua<CR>", { buffer = 0 })
+vim.keymap.set("x", "<leader>ex", ":lua<CR>", { buffer = 0 })
+vim.keymap.set("n", "<leader>eu", ":source %<CR>", { buffer = 0 })
