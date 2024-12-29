da0aa23e4401658d62518ac9cbb39f691a6419a7
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-12-29 00:08:33 -0600

    feat(nvim): Map a way to escape terminal mode

--- .config/nvim/lua/me/remaps.lua
+++ .config/nvim/lua/me/remaps.lua
@@ -40,6 +40,7 @@ vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("t", "<C-o>", "<C-\\><C-n>")
 
 vim.keymap.set("n", "tq", function()
 	local qf_exists = false
