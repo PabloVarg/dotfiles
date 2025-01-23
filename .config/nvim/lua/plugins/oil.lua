6f7c64e23f45eae1b01d956631b5712a0551baf0
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2025-01-22 22:15:29 -0600

    feat(nvim-oil): Configure keymaps

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -8,6 +8,24 @@ return {
 					padding = 2,
 					max_width = 80,
 				},
+				use_default_keymaps = false,
+				keymaps = {
+					["g?"] = { "actions.show_help", mode = "n" },
+					["<CR>"] = "actions.select",
+					["<C-v>"] = { "actions.select", opts = { vertical = true } },
+					["<C-s>"] = { "actions.select", opts = { horizontal = true } },
+					["<C-p>"] = "actions.preview",
+					["<C-c>"] = { "actions.close", mode = "n" },
+					["<C-r>"] = "actions.refresh",
+					["-"] = { "actions.parent", mode = "n" },
+					["_"] = { "actions.open_cwd", mode = "n" },
+					["`"] = { "actions.cd", mode = "n" },
+					["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+					["gs"] = { "actions.change_sort", mode = "n" },
+					["gx"] = "actions.open_external",
+					["g."] = { "actions.toggle_hidden", mode = "n" },
+					["g\\"] = { "actions.toggle_trash", mode = "n" },
+				},
 			})
 
 			vim.keymap.set("n", "<leader>-", require("oil").toggle_float, { silent = true })
