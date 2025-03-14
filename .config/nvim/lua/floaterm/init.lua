9aaa324f7aaa5c5d955ead97da216abd8bf6c92a
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2025-03-13 18:12:43 -0600

    feat(nvim): Set default options for floaterm plugin

--- .config/nvim/lua/floaterm/init.lua
+++ .config/nvim/lua/floaterm/init.lua
@@ -1,20 +1,27 @@
 local Terminal = {
+	default_width_p = 0,
+	default_height_p = 0,
 	min_width = 0,
 	buf = -1,
 	win = -1,
 }
 
-function Terminal:toggle(width_percentage, height_percentage)
+function Terminal:toggle(opts)
+	opts = opts or {}
+
 	if vim.api.nvim_win_is_valid(self.win) then
 		vim.api.nvim_win_hide(self.win)
 		return
 	end
 
+	local width_percentage = opts.width_percentage or self.default_width_p
+	local height_percentage = opts.height_percentage or self.default_height_p
+
 	local width = math.max(math.floor(vim.o.columns * width_percentage), 100)
 	local height = math.floor(vim.o.lines * height_percentage)
 	local col = math.floor((vim.o.columns - width) / 2)
 	local row = math.floor((vim.o.lines - height) / 2)
-	local opts = {
+	local win_opts = {
 		relative = "editor",
 		col = col,
 		row = row,
@@ -29,7 +36,7 @@ function Terminal:toggle(width_percentage, height_percentage)
 		buf = vim.api.nvim_create_buf(false, true)
 	end
 
-	local win = vim.api.nvim_open_win(buf, true, opts)
+	local win = vim.api.nvim_open_win(buf, true, win_opts)
 	vim.wo[win].winhighlight = "Normal:Normal,FloatBorder:Normal"
 	if vim.bo.buftype ~= "terminal" then
 		vim.cmd.terminal()
@@ -42,7 +49,12 @@ function Terminal:toggle(width_percentage, height_percentage)
 end
 
 function Terminal:setup(opts)
+	opts = opts or {}
+
 	self.min_width = opts.min_width or 100
+	self.default_width_p = opts.width_percentage or 0.5
+	self.default_height_p = opts.height_percentage or 0.5
+end
 end
 
 return Terminal
