569fd7caca3d7a8837926f64ffa90d1129a994cf
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2025-03-26 20:55:15 -0600

    feat(nvim): Auto resize floating terminal

--- .config/nvim/lua/floaterm/init.lua
+++ .config/nvim/lua/floaterm/init.lua
@@ -1,11 +1,25 @@
 local Terminal = {
 	default_width_p = 0,
 	default_height_p = 0,
+	curr_width_p = 0,
+	curr_height_p = 0,
 	min_width = 0,
 	buf = -1,
 	win = -1,
 	chan = -1,
 }
+
+function Terminal:calculate_dimensions()
+	local width = math.max(math.floor(vim.o.columns * self.curr_width_p), 100)
+	local height = math.floor(vim.o.lines * self.curr_height_p)
+
+	return {
+		width = width,
+		height = height,
+		col = math.floor((vim.o.columns - width) / 2),
+		row = math.floor((vim.o.lines - height) / 2),
+	}
+end
 
 function Terminal:toggle(opts)
 	opts = opts or {}
@@ -15,21 +29,18 @@ function Terminal:toggle(opts)
 		return
 	end
 
-	local width_percentage = opts.width_percentage or self.default_width_p
-	local height_percentage = opts.height_percentage or self.default_height_p
+	self.curr_width_p = opts.width_percentage or self.default_width_p
+	self.curr_height_p = opts.height_percentage or self.default_height_p
 
-	local width = math.max(math.floor(vim.o.columns * width_percentage), 100)
-	local height = math.floor(vim.o.lines * height_percentage)
-	local col = math.floor((vim.o.columns - width) / 2)
-	local row = math.floor((vim.o.lines - height) / 2)
+	local dims = self:calculate_dimensions()
 	local win_opts = {
 		relative = "editor",
-		col = col,
-		row = row,
-		width = width,
-		height = height,
 		style = "minimal",
 		border = "single",
+		col = dims.col,
+		row = dims.row,
+		width = dims.width,
+		height = dims.height,
 	}
 
 	local buf = self.buf
@@ -68,5 +79,25 @@ function Terminal:send(opts)
 
 	vim.api.nvim_chan_send(self.chan, cmd .. "\n")
 end
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+	group = vim.api.nvim_create_augroup("floaterm", { clear = true }),
+	callback = function()
+		if not vim.api.nvim_win_is_valid(Terminal.win) then
+			return
+		end
+
+		local dims = Terminal:calculate_dimensions()
+		vim.api.nvim_win_set_config(Terminal.win, {
+			relative = "editor",
+			style = "minimal",
+			border = "single",
+			col = dims.col,
+			row = dims.row,
+			width = dims.width,
+			height = dims.height,
+		})
+	end,
+})
 
 return Terminal
